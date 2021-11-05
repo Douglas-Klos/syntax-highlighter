55a8d9f1a667702b11be878fbc5b6e2f3118566a
Douglas Klos <dougklos@gmail.com>
2021-11-04 22:12:58 -0700

    Functional single page prototype

--- app.py
+++ app.py
@@ -11,45 +11,21 @@ app.secret_key = b'#6\xbe\xdf\xaf\xbf\xf6\xef\xa1X\xfe\xda#\xc1\x97\x1d\xb4\xad\
 
 user_input = ""
 
-""" Two Pages """
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
 
-#     if request.method == 'GET':
-#         return render_template("user_input.html")
-
-#     if request.method == 'POST':
-#         session['user_input'] = request.form["user_input"]
-#         return redirect(url_for('format_input'))
-
-
-# @app.route('/syntax', methods=['GET', 'POST'])
-# def format_input():
-
-#     if request.method == 'GET':
-#         return render_template(
-#             'highlighted.html',
-#             user_input=highlight(
-#                 session['user_input'],
-#                 PythonLexer(),
-#                 HtmlFormatter(),
-#             )
-#         )
-    
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'GET':
-        return render_template("user_input.html")
+        return render_template("syntax_highlighting.jinja2")
 
     if request.method == 'POST':
         session['user_input'] = request.form["user_input"]
 
         return render_template(
-                    'user_input.html',
+                    'syntax_highlighting.jinja2',
                     user_input=highlight(
                         session['user_input'],
                         PythonLexer(),
-                        HtmlFormatter(),
+                        HtmlFormatter(linenos='table'),
                     )
                 )
